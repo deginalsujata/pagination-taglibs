cacd83efda4dec603f693970d78b674c31fdb48d
paginationspring <paginationspring@gmail.com>
2014-02-12 22:38:48 +0800

    Dummy for gen fakejavadoc

---
+++ src/main/java/com/github/paginationspring/dummy/PaginationDummy.java
@@ -0,0 +1,12 @@
+package com.github.paginationspring.dummy;
+
+public class PaginationDummy {
+
+    /**
+     * No need java doc in this package
+     * @return true
+     */
+    public boolean noNeedJavaDoc() {
+        return true;
+    }
+}
